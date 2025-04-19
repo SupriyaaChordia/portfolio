d2d15bed13e5a8765e9ebed532526badb8d88c1c
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-19 16:09:57 -0700

    step3

--- global.js
+++ global.js
@@ -32,5 +32,12 @@ for (let p of pages) {
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+      );
+    a.toggleAttribute('target', a.host !== location.host);
+    a.toggleAttribute('rel', a.host !== location.host);
     nav.append(a);
 }
+
