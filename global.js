0e3eb79bb25763c9e1a253d6b639f23112780f97
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-19 15:12:23 -0700

    step3

--- global.js
+++ global.js
@@ -3,11 +3,11 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 };
-navLinks = $$("nav a")
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-  currentLink?.classList.add('current');
+// navLinks = $$("nav a")
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+//   currentLink?.classList.add('current');
 
 let pages = [
 { url: '', title: 'Home' },
