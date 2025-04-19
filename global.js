2d3475a623ea4366bbddf1dac299b65cc7d7571f
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-19 15:16:57 -0700

    step3

--- global.js
+++ global.js
@@ -23,18 +23,13 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    // next step: create link and add it to nav
+    // Create link and add it to nav
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
 
-
-// Create link and add it to nav
-
-
-// const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-//   ? "/"                  // Local server
-//   : "/website/";         // GitHub Pages repo name
-
-// url = !url.startsWith('http') ? BASE_PATH + url : url;
-
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+url = !url.startsWith('http') ? BASE_PATH + url : url;
