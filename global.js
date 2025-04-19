f1124f371e9b40c42ac592c49641a3d625448661
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-19 15:01:03 -0700

    step3

--- global.js
+++ global.js
@@ -8,3 +8,30 @@ let currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname,
   );
   currentLink?.classList.add('current');
+
+let pages = [
+{ url: '', title: 'Home' },
+{ url: 'projects/', title: 'Projects' },
+{ url: 'resume/', title: 'Resume' },
+{ url: 'contact/', title: 'Contact' },
+{ url: 'https://github.com/SupriyaaChordia', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+}
+
+// Create link and add it to nav
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+
