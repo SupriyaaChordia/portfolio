c0a40472b6ba3303ad6997c8a24ea0de57a52d37
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-25 18:53:05 -0700

    step3

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ async function loadProjects() {
 // so I wrapped it in an async function
 loadProjects();
 
-const githubData = await fetchGitHubData('giorgianicolaou');
-const profileStats = document.querySelector('#profile-stats');
-if (profileStats) {
-    profileStats.innerHTML = `
-        <dl>
-            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-            <dt>Followers:</dt><dd>${githubData.followers}</dd>
-            <dt>Following:</dt><dd>${githubData.following}</dd>
-        </dl>
-    `;
+async function loadGitHubProfile() {
+    const githubData = await fetchGitHubData('SupriyaaChordia');
+    const profileStats = document.querySelector('#profile-stats');
+    if (profileStats) {
+        profileStats.innerHTML = `
+            <dl>
+                <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+                <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+                <dt>Followers:</dt><dd>${githubData.followers}</dd>
+                <dt>Following:</dt><dd>${githubData.following}</dd>
+            </dl>
+        `;
+    }
 }
+loadGitHubProfile();
