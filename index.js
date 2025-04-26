ee90848f52873dd39b5cf88b948f839e301e9ab8
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-25 18:40:46 -0700

    testing

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 import { fetchJSON, renderProjects } from './global.js';
-
-console.log('index.js loaded ✅');
-
 async function loadProjects() {
     const projects = await fetchJSON('./lib/projects.json');
-    console.log('Fetched projects:', projects);
-
     const latestProjects = projects.slice(0, 3);
-    console.log('Latest 3 projects:', latestProjects);
-
     const projectsContainer = document.querySelector('.projects');
-    console.log('Projects container:', projectsContainer);
-
     renderProjects(latestProjects, projectsContainer, 'h2');
 }
-
-// Call the async function
+// I was getting an error with just await, on researhcing I found that an async function is needed
+// so I wrapped it in an async function
 loadProjects();
