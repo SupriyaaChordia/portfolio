2e778bab8c0448e2929b614ca1369c554b1ae239
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-04-25 18:05:48 -0700

    step 1.6

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
+console.log('Fetched projects:', projects);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const titleElement = document.querySelector('h1');
