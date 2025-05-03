8e5316cca3d4bd1c8d1215beb2d050b33d2c7f86
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-05-02 23:27:15 -0700

    cleaned

--- projects/projects.js
+++ projects/projects.js
@@ -9,83 +9,8 @@ if (titleElement) {
 }
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-// let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-// let arc = arcGenerator({
-//   startAngle: 0,
-//   endAngle: 2 * Math.PI,
-// });
-// d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-// let data = [1, 2, 3, 4, 5, 5
-// let data = [
-//   { value: 1, label: 'apples' },
-//   { value: 2, label: 'oranges' },
-//   { value: 3, label: 'mangos' },
-//   { value: 4, label: 'pears' },
-//   { value: 5, label: 'limes' },
-//   { value: 5, label: 'cherries' },
-// ];
-// let rolledData = d3.rollups(
-//   projects,
-//   (v) => v.length,
-//   (d) => d.year,
-// );
-// let data = rolledData.map(([year, count]) => {
-//   return { value: count, label: year };
-// });
-// // let total = 0;
-// // let colors = ['gold', 'purple'];
 let colors = d3.scaleOrdinal(d3.schemeSet3);
-
-// for (let d of data) {
-//   total += d;
-// }
-// let angle = 0;
-// let arcData = [];
-
-// for (let d of data) {
-//   let endAngle = angle + (d / total) * 2 * Math.PI;
-//   arcData.push({ startAngle: angle, endAngle });
-//   angle = endAngle;
-// }
-// let arcs = arcData.map((d) => arcGenerator(d));
-// arcs.forEach((arc, idx) => {
-//   d3.select('svg')
-//     .append('path')
-//     .attr('d', arc)
-//     .attr(...) // Fill in the attribute for fill color via indexing the colors variable
-// })
-// let sliceGenerator = d3.pie();
-// let sliceGenerator = d3.pie().value((d) => d.value);
-// let arcData = sliceGenerator(data);
-// let arcs = arcData.map((d) => arcGenerator(d));
-// arcs.forEach((arc, index) => {
-//   d3.select('svg')
-//     .append('path')
-//     .attr('d', arc)
-//     .attr('fill', colors(index)) // Fill in the attribute for fill color via indexing the colors variable
-// })
-// let legend = d3.select('.legend');
-// data.forEach((d, idx) => {
-//   legend
-//     .append('li')
-//     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-//     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`) // set the inner html of <li>
-//     .attr('class', 'legend-item');
-// });
-// let query = '';
 let searchInput = document.querySelector('.searchBar');
-// searchInput.addEventListener('change', (event) => {
-//   // update query value
-//   query = event.target.value;
-//   // filter projects
-//   let filteredProjects = projects.filter((project) => {
-//     let values = Object.values(project).join('\n').toLowerCase();
-//     return values.includes(query.toLowerCase());
-//   });
-//   // render filtered projects
-//   renderProjects(filteredProjects, projectsContainer, 'h2');
-// });
-
 let selectedIndex = -1;
 let query = '';
 
