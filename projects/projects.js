840a2cbe9b1b71653ef6f077db58db3af8cbaa8d
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-05-02 17:45:43 -0700

    step2.2

--- projects/projects.js
+++ projects/projects.js
@@ -15,10 +15,18 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 //   endAngle: 2 * Math.PI,
 // });
 // d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-let data = [1, 2, 3, 4, 5, 5];
+// let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 // let total = 0;
 // let colors = ['gold', 'purple'];
-let colors = d3.scaleOrdinal(d3.schemePastel1);
+let colors = d3.scaleOrdinal(d3.schemeSet3);
 
 // for (let d of data) {
 //   total += d;
@@ -38,7 +46,8 @@ let colors = d3.scaleOrdinal(d3.schemePastel1);
 //     .attr('d', arc)
 //     .attr(...) // Fill in the attribute for fill color via indexing the colors variable
 // })
-let sliceGenerator = d3.pie();
+// let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 arcs.forEach((arc, index) => {
@@ -47,3 +56,11 @@ arcs.forEach((arc, index) => {
     .attr('d', arc)
     .attr('fill', colors(index)) // Fill in the attribute for fill color via indexing the colors variable
 })
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`) // set the inner html of <li>
+    .attr('class', 'legend-item');
+});
