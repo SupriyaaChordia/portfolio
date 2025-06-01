818c61867b6f9c33903aaee5cf31f9fcdd5adad6
SupriyaaChordia <160193935+SupriyaaChordia@users.noreply.github.com>
2025-05-31 21:04:48 -0700

    debug

--- meta/main.js
+++ meta/main.js
@@ -1,4 +1,6 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
+
 
 let xScale, yScale;
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
@@ -311,6 +313,40 @@ async function init() {
 
   document.getElementById("commit-progress").addEventListener("input", onTimeSliderChange);
   onTimeSliderChange();
+
+  d3.select('#scatter-story')
+  .html('')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`,
+  );
+
+const scroller = scrollama();
+scroller
+  .setup({
+    container: '#scrolly-1',
+    step: '#scrolly-1 .step',
+  })
+  .onStepEnter(onStepEnter);
 }
 
 let filteredCommits = commits;
@@ -418,45 +454,9 @@ function updateFileDisplay(filteredCommits) {
     .attr('style', (d) => `--color: ${colors(d.type)}`);
 }
 
-d3.select('#scatter-story')
-  .html('')
-  .selectAll('.step')
-  .data(commits)
-  .join('div')
-  .attr('class', 'step')
-  .html(
-    (d, i) => `
-		On ${d.datetime.toLocaleString('en', {
-      dateStyle: 'full',
-      timeStyle: 'short',
-    })},
-		I made <a href="${d.url}" target="_blank">${
-      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
-    }</a>.
-		I edited ${d.totalLines} lines across ${
-      d3.rollups(
-        d.lines,
-        (D) => D.length,
-        (d) => d.file,
-      ).length
-    } files.
-		Then I looked over all I had made, and I saw that it was very good.
-	`,
-  );
-
-import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
-
 function onStepEnter(response) {
   const currentTime = response.element.__data__.datetime;
   const filtered = commits.filter((d) => d.datetime <= currentTime);
   updateScatterPlot(data, filtered);
   updateFileDisplay(filtered);
 }
-
-const scroller = scrollama();
-scroller
-  .setup({
-    container: '#scrolly-1',
-    step: '#scrolly-1 .step',
-  })
-  .onStepEnter(onStepEnter);
